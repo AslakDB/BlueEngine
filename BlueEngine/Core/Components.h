1d6bd05f6891c6710e443b9bd090af7b4c866f39
AslakDB <Askelakken@gmail.com>
2024-10-07 12:18:43 +0200

    Added entity class
    Next step, create manager for entities and systems

--- BlueEngine/Core/Components.h
+++ BlueEngine/Core/Components.h
@@ -7,13 +7,7 @@
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include "Vertex.h"
-
-class Entity
-{
-public:
-    int ID  = 0;
-    Entity(int id) : ID(id) {}
-};
+#include "Entity"
 
 class Components
 {
@@ -23,6 +17,7 @@ public:
 
 struct i_component_handler 
 {
+    virtual ~i_component_handler() = default;
 };
 
  template <typename T> 
@@ -47,19 +42,45 @@ struct component_manager
         handler->Components.emplace_back();
         handler->index[id] = handler->Components.size() - 1;
     }
+
+    template <typename T>
+    void remove_component(unsigned int id)
+    {
+        std::string component_name = typeid(T).name();
+        if (component_map.contains(component_name) == false)
+        {
+            return;
+        }
+        component_handler<T>* handler = static_cast<component_handler<T>*>(component_map[component_name]);
+        int idx = handler->index[id];
+        
+        handler->Components.erase(handler->Components.begin() + idx);
+        handler->index.erase(id);
+
+        for (auto& [key, value] : handler->index)
+        {
+            if (value > idx)
+            {
+                --value;
+            }
+        }
+    }
     
 };
 
 
-struct position_component : public Components
+/*struct position_component : public Components
 {
-    glm::vec3 PlayerPos = glm::vec3(0.f);
-};
+    
+};*/
 
 struct transform_component : public Components
 {
+    glm::vec3 PlayerPos = glm::vec3(0.f);
     glm::vec3 Scale = glm::vec3(0.f);
     glm::vec3 Rotation = glm::vec3(0.f);
+
+    
 };
 
 struct movement_component : public Components
--- BlueEngine/Core/Systems.h
+++ BlueEngine/Core/Systems.h
@@ -1,20 +1,29 @@
 ﻿#pragma once
+#include <set>
+
 #include "Components.h"
 class Systems
 {
 public:
    virtual void Update();
+    std::pmr::set<Entity> mEntities;
+};
+
+class SystemManager
+{
+    
 };
 
 struct movementSystem : public Systems
 {
-    position_component *position;
+    /*position_component *position;*/
     transform_component *transform;
     movement_component *movement;
     void Update() override
     {
-        position->PlayerPos += movement->Velocity * movement->speed;
+        transform->PlayerPos += movement->Velocity * movement->speed;
     }
+
 };
 
 struct HealthSystem : public Systems
@@ -30,12 +39,12 @@ struct HealthSystem : public Systems
 struct matrix_system : public Systems
 {
     matrix_component *matrix;
-    position_component *position;
+    //position_component *position;
     transform_component *transform;
     void Update() override
     {
         matrix->ModelMatrix = glm::mat4(1.f);
-        matrix->ModelMatrix = glm::translate(matrix->ModelMatrix, position->PlayerPos);
+        matrix->ModelMatrix = glm::translate(matrix->ModelMatrix, transform->PlayerPos);
         matrix->ModelMatrix = glm::rotate(matrix->ModelMatrix, transform->Rotation.x, glm::vec3(1.f, 0.f, 0.f));
         matrix->ModelMatrix = glm::rotate(matrix->ModelMatrix, transform->Rotation.y, glm::vec3(0.f, 1.f, 0.f));
         matrix->ModelMatrix = glm::rotate(matrix->ModelMatrix, transform->Rotation.z, glm::vec3(0.f, 0.f, 1.f));
